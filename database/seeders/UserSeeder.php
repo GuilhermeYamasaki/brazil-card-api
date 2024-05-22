1555bc3a6cc8dafe03b2fed30f4fc77a05851616
Guilherme Yamasaki <guilhermeyyamasaki1@gmail.com>
2024-05-22 18:15:07 -0300

    fix: run pint

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,12 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
 {
-
     public function run(): void
     {
         User::factory()->count(10)->create();
