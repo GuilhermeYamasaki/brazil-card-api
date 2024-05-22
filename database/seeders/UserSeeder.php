0ea866c50888d5ff3603a3a0f084dc76dd2dffe3
Guilherme Yamasaki <guilhermeyyamasaki1@gmail.com>
2024-05-22 18:09:27 -0300

    feat: add user seeders

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+
+    public function run(): void
+    {
+        User::factory()->count(10)->create();
+    }
+}
