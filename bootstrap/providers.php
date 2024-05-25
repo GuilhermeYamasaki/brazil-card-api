1bba7be2a7f7a5576bac88238bba0468497f8d7a
Guilherme Yamasaki <guilhermeyyamasaki1@gmail.com>
2024-05-24 21:53:59 -0300

    feat: add lib telescope

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,10 +2,11 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\Repositories\UserRepositoryProvider::class,
-    App\Providers\Services\UserServiceProvider::class,
-    App\Providers\Services\AsaasServiceProvider::class,
     App\Providers\Repositories\AsaasRepositoryProvider::class,
     App\Providers\Repositories\TransactionRepositoryProvider::class,
+    App\Providers\Repositories\UserRepositoryProvider::class,
+    App\Providers\Services\AsaasServiceProvider::class,
     App\Providers\Services\TransactionServiceProvider::class,
+    App\Providers\Services\UserServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
 ];
