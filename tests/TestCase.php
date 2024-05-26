dc8154c0b47c2f0a297e7e05bc55e533dbfafa3e
Guilherme Yamasaki <guilhermeyyamasaki1@gmail.com>
2024-05-26 14:13:14 -0300

    feat: add generateHeaderToken method to TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,18 @@
 
 namespace Tests;
 
+use App\Models\User;
+use App\Services\GenerateTokenService;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    public function generateHeaderToken(User $user): array
+    {
+        $token = resolve(GenerateTokenService::class)->handle($user);
+
+        return [
+            'Authorization' => "Bearer {$token}",
+        ];
+    }
 }
