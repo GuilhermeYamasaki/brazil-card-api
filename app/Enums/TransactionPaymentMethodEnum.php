7443dd8c5546784f14ce770732631d19b64f4c60
Guilherme Yamasaki <guilhermeyyamasaki1@gmail.com>
2024-05-26 14:10:08 -0300

    feat: add enums for transaction payment methods and status

--- app/Enums/TransactionPaymentMethodEnum.php
+++ app/Enums/TransactionPaymentMethodEnum.php
@@ -10,12 +10,29 @@ enum TransactionPaymentMethodEnum: int
 
     case PIX = 1;
     case CREDIT_CARD = 2;
+    case BOLETO = 3;
+    case UNDEFINED = 4;
+    case DEBIT_CARD = 5;
 
     public function asaas()
     {
         return match ($this) {
             self::PIX => 'PIX',
             self::CREDIT_CARD => 'CREDIT_CARD',
+            self::BOLETO => 'BOLETO',
+            self::UNDEFINED => 'UNDEFINED',
+            self::DEBIT_CARD => 'DEBIT_CARD',
+        };
+    }
+
+    public static function hydrateAsaas(string $asaasPaymentMethod): self
+    {
+        return match ($asaasPaymentMethod) {
+            'PIX' => self::PIX,
+            'CREDIT_CARD' => self::CREDIT_CARD,
+            'BOLETO' => self::BOLETO,
+            'UNDEFINED' => self::UNDEFINED,
+            'DEBIT_CARD' => self::DEBIT_CARD,
         };
     }
 }
--- app/Enums/TransactionStatusEnum.php
+++ app/Enums/TransactionStatusEnum.php
@@ -11,5 +11,17 @@ enum TransactionStatusEnum: int
     case PENDING = 1;
     case PAID = 2;
     case REFUND = 3;
-    case CANCELED = 4;
+    case CONFIRMED = 4;
+    case OVERDUE = 5;
+
+    public static function hydrateAsaas(string $asaasStatus): self
+    {
+        return match ($asaasStatus) {
+            'PENDING' => self::PENDING,
+            'RECEIVED' => self::PAID,
+            'REFUNDED' => self::REFUND,
+            'CONFIRMED' => self::CONFIRMED,
+            'OVERDUE' => self::OVERDUE
+        };
+    }
 }
